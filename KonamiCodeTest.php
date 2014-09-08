cf456cbafce20c9a00ab37515f58baf89bcc153f
Christopher Schmitz <schmitz.chris@gmail.com>
2014-09-08 08:44:58 -0500

    Adding the konami code files

---
+++ KonamiCodeTest.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8"> 
+<head>
+<title>Konami Code Test</title>
+<style>
+    body,html{height:98%;width:98%;}
+</style>
+<script src="kcontrol.js"></script>
+</head>
+<body>
+    <div id="">Lorem ipsum dolor sit amet, vim id libris epicuri, dolorum consequat mediocritatem vix eu. Probatus elaboraret instructior pro an, quo ne idque omnium, at vel dicit labore. Nam eu brute melius iisque, explicari sententiae ei nam, offendit facilisi voluptaria in vel. Dolorem forensibus sit te, et has clita dignissim gubergren.
+
+Et eos veri quodsi appetere, an qui saepe malorum eloquentiam. Vis ut agam iusto lobortis. Ex nam ullum fuisset lucilius. Quod postulant eos ut, insolens adipiscing contentiones ius no, per oratio tantas vocent an.
+
+Ei nec populo scriptorem efficiantur, exerci inermis no usu. Mei adhuc inermis intellegebat eu. Nec stet indoctum ocurreret ei, velit quando veniam nec et, an graece vidisse vel. Apeirian theophrastus nam ne, ut fugit docendi sit. Ut nec platonem constituam, vim facer constituam in, detraxit mnesarchum mediocritatem per ei.
+
+<table id="" >
+    <tr>
+        <td>item 1</td>
+    </tr>
+    <tr>
+        <td>item 2</td>
+    </tr>
+</table>
+</div> 
+
+</body>
+</html>
---
+++ kcontrol.js
@@ -0,0 +1,68 @@
+document.write('<span id="kdiv"><table id="ktable"><tr><td>•</td><td>•</td><td>•</td><td>•</td><td>•</td><td>•</td><td>•</td><td>•</td><td>•</td><td>•</td><td>•</td></tr></table></span>');
+
+var currentCode;
+var fullCode;
+var indicatorRow;
+
+document.onkeydown = function (event){
+        kcontrol(event.keyCode);
+};
+
+
+var init = setInterval(function (){
+    if (document.readyState == "complete"){
+        styleKDiv();
+        currentCode  = Array();
+        fullCode     = Array(38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13);
+        indicatorRow = document.getElementById("ktable").getElementsByTagName("tr");
+        clearInterval(init);
+    }
+}, 10);
+
+function styleKDiv(){
+    var kdiv = document.getElementById("kdiv");
+    kdiv.style.position = "fixed";
+    kdiv.style.bottom = "3px";
+    kdiv.style.right = "3px";
+
+    var ktable = document.getElementById("ktable");
+    ktable.style.backgroundColor = "#242424";
+    ktable.style.color = "white";
+    ktable.style.border = "1px solid #242424";
+    ktable.style.borderRadius = "5px";
+}
+
+function kcontrol(key){
+    currentCode.push(key);
+    if (currentCode[currentCode.length - 1] != fullCode[currentCode.length - 1]){
+        currentCode = [];
+        clearIndicator();
+    } else if (currentCode.length == fullCode.length) {
+        updateIndicator(currentCode.length - 1);
+        // fire function at the end of a successful konami code entering
+        awwwYeah();
+        currenCode = [];
+        clearIndicator();
+    } else {
+        updateIndicator(currentCode.length - 1);
+    }
+}
+
+function updateIndicator(currentIndicatorPosition){
+    var currentIndicator = indicatorRow[0].children[currentIndicatorPosition].style.color="red";
+}
+
+function clearIndicator(){
+    var currentIndicator;
+    for (i = 0; i < fullCode.length ; i++ ){
+        currentIndicator = indicatorRow[0].children[fullCode.length - 1 - i];
+        currentIndicator.style.color="white";
+    }
+
+}
+
+function awwwYeah(){
+    var body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor = "green";
+    body.style.color = "white";
+}
